229a19066d43cbc9530e471da0dda69cb55ee574
Deepak Pattapu <Deepakpattapu@gmail.com>
2026-01-25 00:45:04 -0600

    changed the heading for far shott and the turn speed for eli

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/software/Controller.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/software/Controller.java
@@ -138,7 +138,7 @@ public class Controller extends Gamepad {
 	public final double getProcessedValue(Action action) {
 		double val = getProcessedValue(getControlForAction(action));
 		if (action == Action.ROTATE_LEFT || action == Action.ROTATE_RIGHT) {
-			val /= 2;
+			val /= 5;
 		}
 		if (action == Action.SLOW_FORWARD || action == Action.SLOW_BACKWARD || action == Action.SLOW_LEFT
 				|| action == Action.SLOW_RIGHT) {
